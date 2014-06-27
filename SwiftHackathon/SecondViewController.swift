a0885e00a54e2cbde6226048f6a0e78a30dc4a35
Jordan Zucker <jzucker@twitter.com>
2014-06-27 10:41:05 -0700

    Initial Commit

---
+++ SwiftHackathon/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SwiftHackathon
+//
+//  Created by Jordan Zucker on 6/27/14.
+//  Copyright (c) 2014 Jordan Zucker. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
